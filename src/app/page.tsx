4df95192f15993d44ccdcab807266ec496d6a13b
Muhammad Aleem Raza <aleemraza661@gmail.com>
2025-03-19 21:07:25 +0500

    bull-street loading-screen desgin commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 "use client"
-
 import dynamic from "next/dynamic";
+import HeroSection from "./components/Home/HeroSection";
 //import "reactjs-tiptap-editor/style.css";
-
 const ReactEditor = dynamic(() => import("@/app/components/ReactEditor"), { ssr: false });
 
 export default function Home() {
   return (
-    <div>
-      <ReactEditor/>
+    <div className="">
+      <HeroSection/>
+      {/* <ReactEditor/> */}
     </div>
   );
 }
